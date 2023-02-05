52d3137341205ff91d9c5787409f80249a9e50c7
allaberx <allaberdinx@gmail.com>
2023-02-06 02:48:13 +0500

    lesson 8

--- app/src/main/java/com/allaber/shoppinglist/presentation/ShopListAdapter.kt
+++ app/src/main/java/com/allaber/shoppinglist/presentation/ShopListAdapter.kt
@@ -4,32 +4,48 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.allaber.shoppinglist.R
 import com.allaber.shoppinglist.domain.ShopItem
 
 class ShopListAdapter : RecyclerView.Adapter<ShopListAdapter.ShopItemViewHolder>() {
 
-    val list = listOf<ShopItem>()
+    var shopList = listOf<ShopItem>()
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ShopItemViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_shop_disabled, parent, false)
+        val view = LayoutInflater.from(parent.context).inflate(
+            R.layout.item_shop_disabled,
+            parent,
+            false
+        )
+
         return ShopItemViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: ShopItemViewHolder, position: Int) {
-        val shopItem = list[position]
-        holder.tvName.text = shopItem.name
+        val shopItem = shopList[position]
+
+        val status = if(shopItem.enabled) {
+            "Active"
+        } else {
+            "Not active"
+        }
+
+        holder.tvName.text = "${shopItem.name} $status"
         holder.tvCount.text = shopItem.count.toString()
 
-        holder.view.setOnLongClickListener{
+        holder.view.setOnLongClickListener {
             true
         }
     }
 
     override fun getItemCount(): Int {
-        return list.size
+        return shopList.size
     }
 
     class ShopItemViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
